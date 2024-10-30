760632a9ed4fb5d8f09df60e96ee5c43a9871ba9
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2024-10-30 21:41:19 +0530

    Update ImageService.java

--- src/main/java/com/sujan/tech/dream_shop/service/image/ImageService.java
+++ src/main/java/com/sujan/tech/dream_shop/service/image/ImageService.java
@@ -4,9 +4,10 @@ import com.sujan.tech.dream_shop.model.Image;
 import org.springframework.web.multipart.MultipartFile;
 
 public class ImageService implements IImageService{
+    private final ImageRepository imageRepository;
     @Override
     public Image getImageById(Long id) {
-        return null;
+        return imageRepository.findById();
     }
 
     @Override
