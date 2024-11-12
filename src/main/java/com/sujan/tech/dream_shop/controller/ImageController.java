2019feae2aa6286526b03c2d1dc81da8a62a59a8
SujanBoopathy <sujansaradha@gmail.com>
2024-11-12 09:57:17 +0530

    spring data

--- src/main/java/com/sujan/tech/dream_shop/controller/ImageController.java
+++ src/main/java/com/sujan/tech/dream_shop/controller/ImageController.java
@@ -1,7 +1,36 @@
 package com.sujan.tech.dream_shop.controller;
 
+import com.sujan.tech.dream_shop.dto.ImageDto;
+import com.sujan.tech.dream_shop.respone.ApiResponse;
+import com.sujan.tech.dream_shop.service.image.IImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("${api.prefix}/images")
 public class ImageController {
+    private final IImageService imageService;
+
+    public ResponseEntity<ApiResponse> saveImage(
+            @RequestParam List<MultipartFile> files,
+            @RequestParam Long productId
+    ){
+        try{
+            List<ImageDto> imageDtos = imageService.saveImage(files,productId);
+            return ResponseEntity.ok(new ApiResponse("Upload Success",imageDtos));
+        } catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    new ApiResponse("Upload Failed",e.getMessage())
+            );
+        }
+    }
 }
