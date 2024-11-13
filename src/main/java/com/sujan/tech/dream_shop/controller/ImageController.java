bf65191fb5dda0a5970f609082608097178faba6
SujanBoopathy <sujansaradha@gmail.com>
2024-11-13 10:02:22 +0530

    spring data

--- src/main/java/com/sujan/tech/dream_shop/controller/ImageController.java
+++ src/main/java/com/sujan/tech/dream_shop/controller/ImageController.java
@@ -1,17 +1,20 @@
 package com.sujan.tech.dream_shop.controller;
 
 import com.sujan.tech.dream_shop.dto.ImageDto;
+import com.sujan.tech.dream_shop.model.Image;
 import com.sujan.tech.dream_shop.respone.ApiResponse;
 import com.sujan.tech.dream_shop.service.image.IImageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -32,5 +35,13 @@ public class ImageController {
                     new ApiResponse("Upload Failed",e.getMessage())
             );
         }
+    }
+
+    public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException,Exception {
+        Image image = imageService.getImageById(imageId);
+        ByteArrayResource resource = new ByteArrayResource(image.getImage().getBytes(1,(int) image.getImage().length()));
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\""+image.getFileName()+"\"")
+                .body(resource);
     }
 }
