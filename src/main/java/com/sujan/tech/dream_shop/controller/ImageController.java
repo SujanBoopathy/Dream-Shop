8dfdc21ce515fed82020eb046933fc731f86b587
SujanBoopathy <sujansaradha@gmail.com>
2024-11-14 09:49:06 +0530

    spring data

--- src/main/java/com/sujan/tech/dream_shop/controller/ImageController.java
+++ src/main/java/com/sujan/tech/dream_shop/controller/ImageController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.SQLException;
@@ -43,5 +40,24 @@ public class ImageController {
         return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
                 .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\""+image.getFileName()+"\"")
                 .body(resource);
+    }
+
+    public ResponseEntity<ApiResponse> updateImage(
+            @PathVariable Long imageId,
+            @RequestBody MultipartFile file
+    )  {
+        try{
+            Image image = imageService.getImageById(imageId);
+            if(image != null){
+                imageService.updateImage(file,imageId);
+                return ResponseEntity.ok(new ApiResponse("Update success",null));
+            }
+        } catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(),null));
+        }
+       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+               .body(new ApiResponse("Update failed",HttpStatus.INTERNAL_SERVER_ERROR));
+
     }
 }
