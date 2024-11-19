28a52e1c64a443c4da9b06a0ffbe55c852af5748
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2024-11-19 21:50:06 +0530

    Update CategoryController.java

--- src/main/java/com/sujan/tech/dream_shop/controller/CategoryController.java
+++ src/main/java/com/sujan/tech/dream_shop/controller/CategoryController.java
@@ -4,4 +4,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CategoryController {
+
+  @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return  ResponseEntity.ok(new ApiResponse("Found!", categories));
+        } catch (Exception e) {
+           return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error:", INTERNAL_SERVER_ERROR));
+        }
+    }
 }
