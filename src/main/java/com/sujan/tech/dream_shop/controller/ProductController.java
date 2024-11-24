008314c5e639d0366ce395dbd02e178947d96e31
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2024-11-24 20:38:27 +0530

    Update ProductController.java

--- src/main/java/com/sujan/tech/dream_shop/controller/ProductController.java
+++ src/main/java/com/sujan/tech/dream_shop/controller/ProductController.java
@@ -4,6 +4,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("{api_url}/product")
 public class ProductController {
-  private final ProductRepository productRepository;
+  private final IProductService productService;
+
+
+  public void getAllProducts(){
+    return productService.getAllProducts();
+  }
+  
 }
