83424ef353264b0c34d90ec4110815db5429ead8
SujanBoopathy <sujansaradha@gmail.com>
2024-11-27 09:56:36 +0530

    spring data

--- src/main/java/com/sujan/tech/dream_shop/controller/ProductController.java
+++ src/main/java/com/sujan/tech/dream_shop/controller/ProductController.java
@@ -1,16 +1,26 @@
 package com.sujan.tech.dream_shop.controller;
 
+import com.sujan.tech.dream_shop.model.Product;
+import com.sujan.tech.dream_shop.respone.ApiResponse;
+import com.sujan.tech.dream_shop.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("{api_url}/product")
+@RequestMapping("{api.prefix}/products")
 public class ProductController {
   private final IProductService productService;
 
-
-  public void getAllProducts(){
-    return productService.getAllProducts();
+  @GetMapping("/all")
+  public ResponseEntity<ApiResponse> getAllProducts(){
+    List<Product> products = productService.getAllProducts();
+    return ResponseEntity.ok(new ApiResponse("success",products));
   }
   
 }
