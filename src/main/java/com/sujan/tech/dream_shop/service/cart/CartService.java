1cdc32575c5a0c00f1ebf6283f8f508e452e71a7
SujanBoopathy <sujansaradha@gmail.com>
2025-01-06 23:10:55 +0530

    spring data

--- src/main/java/com/sujan/tech/dream_shop/service/cart/CartService.java
+++ src/main/java/com/sujan/tech/dream_shop/service/cart/CartService.java
@@ -1,7 +1,23 @@
 package com.sujan.tech.dream_shop.service.cart;
 
+import com.sujan.tech.dream_shop.model.Cart;
+import com.sujan.tech.dream_shop.repository.CartRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
 public class CartService implements ICartService {
-    public Cart getCart(Long id){
-        
+    private final CartRepository cartRepository;
+
+    public Cart getCart(Long id) {
+        Cart cart = cartRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Cart not found")
+        );
+        BigDecimal totalAmount = cart.getTotalAmount();
+        cart.setTotalAmount(totalAmount);
+        return cartRepository.save(cart);
     }
 }
--- src/main/java/com/sujan/tech/dream_shop/service/cart/ICartService.java
+++ src/main/java/com/sujan/tech/dream_shop/service/cart/ICartService.java
@@ -1,5 +1,7 @@
 package com.sujan.tech.dream_shop.service.cart;
 
+import com.sujan.tech.dream_shop.model.Cart;
+
 public interface ICartService {
     Cart getCart(Long id);
 }
