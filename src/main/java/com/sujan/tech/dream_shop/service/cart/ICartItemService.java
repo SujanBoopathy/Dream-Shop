657bc0d7e169ac03292008ea5299cbd3cfb6caf1
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2025-01-10 23:03:06 +0530

    Update ICartItemService.java

--- src/main/java/com/sujan/tech/dream_shop/service/cart/ICartItemService.java
+++ src/main/java/com/sujan/tech/dream_shop/service/cart/ICartItemService.java
@@ -4,4 +4,8 @@ import com.sujan.tech.dream_shop.model.CartItem;
 
 public interface ICartItemService {
     void addItemToCart(Long cartId,Long productId,int quantity);
+    void removeItemFromCart(Long cartId, Long productId);
+    void updateItemQuantity(Long cartId, Long productId, int quantity);
+
+    CartItem getCartItem(Long cartId, Long productId);
 }
