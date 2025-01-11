ab2f3c251baf5e652d17d5f914e85c8727b51e9a
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2025-01-11 20:42:14 +0530

    Update CartItemService.java

--- src/main/java/com/sujan/tech/dream_shop/service/cart/CartItemService.java
+++ src/main/java/com/sujan/tech/dream_shop/service/cart/CartItemService.java
@@ -6,6 +6,29 @@ public class CartItemService implements ICartItemService{
 
     @Override
     public void addItemToCart(Long cartId, Long productId, int quantity) {
-
+        //1. Get the cart
+        //2. Get the product
+        //3. Check if the product already in the cart
+        //4. If Yes, then increase the quantity with the requested quantity
+        //5. If No, then initiate a new CartItem entry.
+        Cart cart = cartService.getCart(cartId);
+        Product product = productService.getProductById(productId);
+        CartItem cartItem = cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst().orElse(new CartItem());
+        if (cartItem.getId() == null) {
+            cartItem.setCart(cart);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(quantity);
+            cartItem.setUnitPrice(product.getPrice());
+        }
+        else {
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        }
+        cartItem.setTotalPrice();
+        cart.addItem(cartItem);
+        cartItemRepository.save(cartItem);
+        cartRepository.save(cart);
     }
 }
