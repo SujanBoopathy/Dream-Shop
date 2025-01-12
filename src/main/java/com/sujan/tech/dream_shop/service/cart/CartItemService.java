286ef9eacf062155f8f9190511f052abcb0923ff
SujanBoopathy <sujansaradha@gmail.com>
2025-01-12 20:07:16 +0530

    spring data

--- src/main/java/com/sujan/tech/dream_shop/service/cart/CartItemService.java
+++ src/main/java/com/sujan/tech/dream_shop/service/cart/CartItemService.java
@@ -1,11 +1,25 @@
 package com.sujan.tech.dream_shop.service.cart;
 
+import com.sujan.tech.dream_shop.model.Cart;
 import com.sujan.tech.dream_shop.model.CartItem;
+import com.sujan.tech.dream_shop.model.Product;
+import com.sujan.tech.dream_shop.repository.CartIteamRepository;
+import com.sujan.tech.dream_shop.repository.CartRepository;
+import com.sujan.tech.dream_shop.service.product.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class CartItemService implements ICartItemService{
 
+    private final CartService cartService;
+    private final ProductService productService;
+    private final CartIteamRepository cartItemRepository;
+    private final CartRepository cartRepository;
+
     @Override
-    public void addItemToCart(Long cartId, Long productId, int quantity) {
+    public void addItemToCart(Long cartId, Long productId, int quantity) throws Exception {
         //1. Get the cart
         //2. Get the product
         //3. Check if the product already in the cart
@@ -13,6 +27,7 @@ public class CartItemService implements ICartItemService{
         //5. If No, then initiate a new CartItem entry.
         Cart cart = cartService.getCart(cartId);
         Product product = productService.getProductById(productId);
+
         CartItem cartItem = cart.getItems()
                 .stream()
                 .filter(item -> item.getProduct().getId().equals(productId))
@@ -30,5 +45,20 @@ public class CartItemService implements ICartItemService{
         cart.addItem(cartItem);
         cartItemRepository.save(cartItem);
         cartRepository.save(cart);
+    }
+
+    @Override
+    public void removeItemFromCart(Long cartId, Long productId) {
+
+    }
+
+    @Override
+    public void updateItemQuantity(Long cartId, Long productId, int quantity) {
+
+    }
+
+    @Override
+    public CartItem getCartItem(Long cartId, Long productId) {
+        return null;
     }
 }
--- src/main/java/com/sujan/tech/dream_shop/service/cart/ICartItemService.java
+++ src/main/java/com/sujan/tech/dream_shop/service/cart/ICartItemService.java
@@ -3,7 +3,7 @@ package com.sujan.tech.dream_shop.service.cart;
 import com.sujan.tech.dream_shop.model.CartItem;
 
 public interface ICartItemService {
-    void addItemToCart(Long cartId,Long productId,int quantity);
+    void addItemToCart(Long cartId,Long productId,int quantity) throws Exception;
     void removeItemFromCart(Long cartId, Long productId);
     void updateItemQuantity(Long cartId, Long productId, int quantity);
 
