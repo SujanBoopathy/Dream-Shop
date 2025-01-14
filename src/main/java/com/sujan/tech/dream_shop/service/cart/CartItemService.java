7b3b2466478b8ba3ad5044f46ea009064f3b8ba5
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2025-01-14 21:07:43 +0530

    Update CartItemService.java

--- src/main/java/com/sujan/tech/dream_shop/service/cart/CartItemService.java
+++ src/main/java/com/sujan/tech/dream_shop/service/cart/CartItemService.java
@@ -62,6 +62,10 @@ public class CartItemService implements ICartItemService{
 
     @Override
     public CartItem getCartItem(Long cartId, Long productId) {
-        return null;
+        Cart cart = cartService.getCart(cartId);
+        return  cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst().orElseThrow(() -> new ResourceNotFoundException("Item not found"));
     }
 }
