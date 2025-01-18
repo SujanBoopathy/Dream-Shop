4029d568064731f4016075d169d0192b0eb6200e
SujanBoopathy <sujansaradha@gmail.com>
2025-01-18 19:05:43 +0530

    spring data

--- src/main/java/com/sujan/tech/dream_shop/controller/CartController.java
+++ src/main/java/com/sujan/tech/dream_shop/controller/CartController.java
@@ -1,5 +1,30 @@
+package com.sujan.tech.dream_shop.controller;
 
+import com.sujan.tech.dream_shop.model.Cart;
+import com.sujan.tech.dream_shop.respone.ApiResponse;
+import com.sujan.tech.dream_shop.service.cart.ICartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RequiredArgsConstructor
 @RestController
-public class CartController{
-  
+@RequestMapping("${api.prefix}/carts")
+public class CartController {
+    private final ICartService cartService;
+
+    @GetMapping("/{cartId}/my-cart")
+    public ResponseEntity<ApiResponse> getCart(@PathVariable Long cartId) {
+        try {
+            Cart cart = cartService.getCart(cartId);
+            return ResponseEntity.ok(new ApiResponse("Success", cart));
+        } catch (Exception e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 }
