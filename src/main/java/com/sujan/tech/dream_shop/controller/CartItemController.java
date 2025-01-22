439704daa1f3c25b2a68934b5684e1c97c3788af
SujanBoopathy <sujansaradha@gmail.com>
2025-01-22 22:01:42 +0530

    spring data

--- src/main/java/com/sujan/tech/dream_shop/controller/CartItemController.java
+++ src/main/java/com/sujan/tech/dream_shop/controller/CartItemController.java
@@ -1,10 +1,16 @@
 package com.sujan.tech.dream_shop.controller;
 
+import com.sujan.tech.dream_shop.respone.ApiResponse;
 import com.sujan.tech.dream_shop.service.cart.ICartItemService;
 import com.sujan.tech.dream_shop.service.cart.ICartService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RequiredArgsConstructor
 @RestController
@@ -12,4 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class CartItemController {
     private final ICartItemService cartItemService;
     private final ICartService cartService;
+
+    @PostMapping("/item/add")
+    public ResponseEntity<ApiResponse> addItemToCart(@RequestParam(required = false) Long cartId,
+                                                     @RequestParam Long productId,
+                                                     @RequestParam Integer quantity) {
+        try {
+            if (cartId == null) {
+                cartId= cartService.initializeNewCart();
+            }
+            cartItemService.addItemToCart(cartId, productId, quantity);
+            return ResponseEntity.ok(new ApiResponse("Add Item Success", null));
+        } catch (Exception e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 }
