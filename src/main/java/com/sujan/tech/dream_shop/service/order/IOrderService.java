4242c313e76c86e73e97b2a367694997f3e15c8b
SujanBoopathy <sujansaradha@gmail.com>
2025-01-26 21:45:02 +0530

    spring data

---
+++ src/main/java/com/sujan/tech/dream_shop/service/order/IOrderService.java
@@ -0,0 +1,12 @@
+package com.sujan.tech.dream_shop.service.order;
+
+import com.sujan.tech.dream_shop.dto.OrderDto;
+import com.sujan.tech.dream_shop.model.Order;
+
+import java.util.List;
+
+public interface IOrderService {
+    Order placeOrder(Long userId);
+    OrderDto getOrder(Long orderId);
+    List<OrderDto> getUserOrders(Long userId);
+}
