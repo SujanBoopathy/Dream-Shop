77a34111dfce588437605d42a28ca70d322068bf
SujanBoopathy <sujansaradha@gmail.com>
2025-01-28 21:53:53 +0530

    spring data

--- src/main/java/com/sujan/tech/dream_shop/service/order/OrderService.java
+++ src/main/java/com/sujan/tech/dream_shop/service/order/OrderService.java
@@ -2,10 +2,22 @@ package com.sujan.tech.dream_shop.service.order;
 
 import com.sujan.tech.dream_shop.dto.OrderDto;
 import com.sujan.tech.dream_shop.model.Order;
+import com.sujan.tech.dream_shop.repository.OrderRepository;
+import com.sujan.tech.dream_shop.repository.ProductRepository;
+import com.sujan.tech.dream_shop.service.cart.CartService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class OrderService implements IOrderService{
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final CartService cartService;
+    private final ModelMapper modelMapper;
 
     @Override
     public Order placeOrder(Long userId) {
