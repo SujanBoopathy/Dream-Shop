397160bf7b2689845b3f88900032b30873a2fb05
SujanBoopathy <sujansaradha@gmail.com>
2025-02-03 21:06:56 +0530

    spring data

--- src/main/java/com/sujan/tech/dream_shop/controller/OrderController.java
+++ src/main/java/com/sujan/tech/dream_shop/controller/OrderController.java
@@ -1,8 +1,14 @@
 package com.sujan.tech.dream_shop.controller;
 
+import com.sujan.tech.dream_shop.model.Order;
+import com.sujan.tech.dream_shop.respone.ApiResponse;
 import com.sujan.tech.dream_shop.service.order.IOrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -10,4 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("${api.prefix}/orders")
 public class OrderController {
     private final IOrderService orderService;
+
+    @PostMapping("/order")
+    public ResponseEntity<ApiResponse> createOrder(@RequestParam Long userId) {
+        try {
+            Order order =  orderService.placeOrder(userId);
+            return ResponseEntity.ok(new ApiResponse("Item Order Success!", order));
+        } catch (Exception e) {
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Error Occured!", e.getMessage()));
+        }
+    }
 }
