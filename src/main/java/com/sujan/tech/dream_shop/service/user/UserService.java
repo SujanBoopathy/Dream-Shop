8f6976fe5dc60dbad7640d83c4c11d064ab67121
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2025-02-18 22:09:00 +0530

    Update UserService.java

--- src/main/java/com/sujan/tech/dream_shop/service/user/UserService.java
+++ src/main/java/com/sujan/tech/dream_shop/service/user/UserService.java
@@ -1,6 +1,27 @@
+package com.sujan.tech.dream_shop.service.user;
+
+import com.dailycodework.dreamshops.dto.UserDto;
+import com.dailycodework.dreamshops.exceptions.AlreadyExistsException;
+import com.dailycodework.dreamshops.exceptions.ResourceNotFoundException;
+import com.dailycodework.dreamshops.model.User;
+import com.dailycodework.dreamshops.repository.UserRepository;
+import com.dailycodework.dreamshops.request.CreateUserRequest;
+import com.dailycodework.dreamshops.request.UserUpdateRequest;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
 @Service
-public class UserService{
-   public User getUserById(Long userId){
-     return null;
-   }
+@RequiredArgsConstructor
+public class UserService implements IUserService {
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+   
+    @Override
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found!"));
+    }
 }
