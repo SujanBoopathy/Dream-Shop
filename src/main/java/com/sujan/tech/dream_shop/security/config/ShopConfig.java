453ce452925eb30ef76c7cfff611a5178b5b044d
SujanBoopathy <sujansaradha@gmail.com>
2025-03-10 23:04:23 +0530

    spring data

--- src/main/java/com/sujan/tech/dream_shop/security/config/ShopConfig.java
+++ src/main/java/com/sujan/tech/dream_shop/security/config/ShopConfig.java
@@ -1,8 +1,26 @@
+package com.sujan.tech.dream_shop.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import java.util.List;
+
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
 public class ShopConfig {
-    private final ShopUserDetailsService userDetailsService;
-    private final JwtAuthEntryPoint authEntryPoint;
+
+    private static final List<String> SECURED_URLS =
+            List.of("/api/v1/carts/**", "/api/v1/cartItems/**");
+
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
