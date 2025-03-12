85cd8c7e2491be023a8b09f8cf39198197c7342e
SujanBoopathy <sujansaradha@gmail.com>
2025-03-12 21:26:34 +0530

    spring data

--- src/main/java/com/sujan/tech/dream_shop/security/config/ShopConfig.java
+++ src/main/java/com/sujan/tech/dream_shop/security/config/ShopConfig.java
@@ -4,8 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -22,5 +26,16 @@ public class ShopConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return  authConfig.getAuthenticationManager();
+
     }
 }
